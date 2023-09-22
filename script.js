7d5421b0fab91a2a1d710bb485a710b4193a4bab
Lu1705 <145431365+Lu1705@users.noreply.github.com>
2023-09-22 12:26:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let nbAvenger= 14; // variable de type number 
+let nomDeJules= "César"; // variable de type string
+let nbEtoiles= "beaucoup"; // variable type string
+let bienvenue= "welcome"; // type string
+let faceID= false; // type boolean
+let prixOrdi= 1500; // type number
+let nomSeptNains= "grincheux, simplet et les autres"; // type string (on peut également dire que c'est un tableau et donc un type objet)
+let BgTinder= "Coco quel homme quand même"; // type string
+
+console.log (nbAvenger + " " + nomDeJules + " " + nbEtoiles + " " + bienvenue + " " + faceID + " " + prixOrdi + " " + nomSeptNains + " " + BgTinder);
+
+
+let clonesNaruto = nbEtoiles; // string
+let défault= "perfectionniste, acharné et juste trop fort"; // string peut encore une fois etre tableau donc objet
+let prénomAnne= "Anne"; // string
+let PanierAmazon= "des clés usb thinger skate"; // string peut etre objet car tableau
+let guerre100Ans= 100; // number
+let caractéristiqueBière= "taux d'alcool"; // string
+let NextPresident= "Laink et Terracide";
+
+console.log (clonesNaruto + " " + défault + " " + prénomAnne + " " + PanierAmazon + " " + guerre100Ans + " " + caractéristiqueBière + " " + NextPresident);
+
+
